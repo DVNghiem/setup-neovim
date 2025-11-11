a24859894806bdf20da4ce6323026f6d4f656ffb
DANG VAN NGHIEM <vannghiem848@gmail.com>
2025-11-11 21:46:16 +0700

    refactor: Enhance venv-selector configuration with improved options and key mappings

--- lua/config/plugins/venv-selector.lua
+++ lua/config/plugins/venv-selector.lua
@@ -1,15 +1,46 @@
 return {
     "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    dependencies = {"neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python"},
+    event = "VeryLazy",
+    keys = {{
+        "<leader>vs",
+        "<cmd>VenvSelect<CR>",
+        desc = "Select Virtual Env"
+    }, {
+        "<leader>vc",
+        "<cmd>VenvSelectCached<CR>",
+        desc = "Select Cached Env"
+    }},
     opts = {
-        name = {"venv", ".venv", "odoo-env"},
-        path = ".",
-        venvwrapper_path = "./venv",
+        search_workspace = true,
+        search = true,
+
+        auto_refresh = true,
+
+        name = {"venv", ".venv", "odoo-env", "env", ".env"},
+
+        path = vim.fn.getcwd(),
+
+        parents = 3,
+
+        fd_binary_name = "fd",
+        dap_enabled = true,
+
+        notify_on_select = true
     },
-    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-    keys = {
-        { "<leader>vs", "<cmd>VenvSelect<cr>" },
-        { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
-    },
-    
+    config = function(_, opts)
+        require("venv-selector").setup(opts)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "python",
+            callback = function()
+                vim.schedule(function()
+                    local venv = vim.env.VIRTUAL_ENV
+                    if not venv or venv == "" then
+                        vim.cmd("VenvSelectCached")
+                    end
+                end)
+            end
+        })
+    end
 }
