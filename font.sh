7ffe9ab157c6fadd6d3a487c6fe85bef8964b230
Dang Van Nghiem <vannghiem848@gmail.com>
2026-02-16 15:07:12 +0700

    fix: Ensure fonts directory is created before downloading fonts

--- font.sh
+++ font.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p ~/.local/share/fonts
 curl -fLo ~/.local/share/fonts/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
 unzip ~/.local/share/fonts/JetBrainsMono.zip -d ~/.local/share/fonts/
 rm ~/.local/share/fonts/JetBrainsMono.zip
