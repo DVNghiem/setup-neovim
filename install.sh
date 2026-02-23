3e77ae9fa7a484bcbe630d77e4c002b55fda2f04
Dang Van Nghiem <vannghiem848@gmail.com>
2026-02-23 11:46:18 +0700

    fix: update installation script to include Neovim download and setup

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-sudo dnf install fd-find
-sudo dnf install fzf
-sudo dnf copr enable atim/lazygit -y
-sudo dnf install lazygit
 
-sudo dnf install chafa
+curl -LO https://github.com/neovim/neovim/releases/download/v0.11.6/nvim-linux-x86_64.tar.gz 
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz 
+sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+rm nvim-linux-x86_64.tar.gz
+
+sudo dnf install fd-find -y
+sudo dnf install fzf -y
+sudo dnf copr enable atim/lazygit -y
+sudo dnf install lazygit -y
+sudo dnf install chafa -y
 
